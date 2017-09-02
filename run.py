c36618b78f9f1d156ddaa4607e72426674c1146a
Robus <robusgauli@gmail.com>
2017-09-02 21:22:47 +0545

    new websocket client, server

---
+++ run.py
@@ -0,0 +1,177 @@
+import asyncio
+import websockets
+import json
+import functools
+import itertools
+import argparse
+
+import sys
+
+class ServerProfiler:
+    DEFAULT_PORT = 5000
+    DEFAULT_HOST = '0.0.0.0'
+    LOCAL_NODE = 'localhost'
+    
+    @classmethod
+    def create_from_cli_args(cls):
+        return cls(config=None)
+
+    def __init__(self, config=None):
+        #this object will kep track of all the nodes
+        #including the agent nodes and parent Node
+        self.nodes = {}
+        
+        # we will make sure that only one instance of the node
+        #is active at any request to the web socket
+        
+        #get the config object(dict)
+        self.config = self._load_args_config()
+        self._create_app()
+        self.app = None
+
+        self.client_ws = {}
+                        
+    def _load_args_config(self):
+        #return a configuration dictionary
+        config = {}
+        for key, val in vars(self._get_args()).items():
+            if val:
+                key = 'PROFILER_%s' % key.upper()
+                config[key] = val
+        return config
+        
+    
+    def _get_args(self):
+        parser = argparse.ArgumentParser(
+            description='Server Profiler..'
+        )
+        parser.add_argument(
+            '-p', '--port',
+            action='store',
+            type=int,
+            default=None,
+            dest='port',
+            help='port to listen on. Defaults to 5000'
+        )
+        parser.add_argument(
+            '-host',
+            action='store',
+            type=str,
+            default=None,
+            dest='host',
+            help='host to bind to. Defaults to 0.0.0.0 (all interfaces)'
+        )
+        parser.add_argument(
+            '-a', '--agent',
+            action='store_true',
+            dest='agent',			
+            help='Enables client Node mode. This configures the node as a producer'
+        )
+
+        parser.add_argument(
+            '--register-to',
+            action='store',
+            dest='register_to',
+            help='Register this node as a client node to parent node.(Example: 123.234.23.23:5000(Parent\'s Node address)'
+        )
+        parser.add_argument(
+            '-as', '--register-as',
+            action='store',
+            dest='register_as',
+            help='Alias for the agent Node'
+        )
+
+
+        return parser.parse_args()
+    
+    def _create_app(self):
+        #based on the configuration chooose if the node is the host node or
+        # the client node
+        #by default it acts as a host node
+        #until the configuration says it to be a client node
+        is_agent = self.config.get('PROFILER_AGENT')	
+        if is_agent:
+            #print('i am an agent so i need the way to make sense to produce to consumer')
+            parent_host_port = self.config.get('PROFILER_REGISTER_TO')
+            if not parent_host_port:
+                print('Please specify the parent host and port')
+                sys.exit(0)
+            
+
+            self._run_as_agent()
+            #also if this is agent node than it needs to act to know the 
+        else:
+            self._run_as_parent()
+
+    async def producer_handler(self):
+    
+        async with websockets.connect('ws://%s/' % self.config.get('PROFILER_REGISTER_TO')) as websocket:
+            print('Connected to server node %s at port %d' % (websocket.host, websocket.port))
+            while True:
+                try:
+                    await websocket.send('hi i am the client %s' % self.config.get('PROFILER_REGISTER_AS', 'Unknown'))
+                    await asyncio.sleep(3)
+
+                    message = await websocket.recv()
+                    print(message+ 'from the server ')
+                    if message == 'pause':
+                        #keep on waiting until it ask you for more data
+                        while True:
+                            message = await websocket.recv()
+                            if message == 'continue':
+                                break
+                except websockets.exceptions.ConnectionClosed:
+                    print('Connection closed by the parent')
+                    #if the connection is closed by the client
+                    break
+                    #again goes to eh outer loop and starts the connection again
+                except Exception:
+                    print('Connection lost')
+                    break
+
+                
+    async def consumer_producer_handler(self, websocket, path):
+        #wheh someone wants to connect to the server node, it must register itseld as a clients
+        #
+        print('Got connection from client host %s and port%s' % (websocket.host, websocket.port))
+        #print(websocket.origin, websocket.request_headers, websocket.response_headers)
+        print([(key, val) for key, val in websocket.__dict__.items() if not callable(val)])
+        
+        while True:
+            #once the websocket is connected send the continue message
+            #websocket.send('continue')
+            received = await websocket.recv()
+
+            print(received, path)
+            await websocket.send('continue')
+
+    def _run_as_agent(self):
+        pass
+    
+    def _run_as_parent(self):
+        pass
+    
+    def run(self):
+        if self.config.get('PROFILER_AGENT', None):
+            asyncio.get_event_loop().run_until_complete(self.producer_handler())
+        else:
+            #run in the parent mode 
+            #take the configuration from the command line if provied else use default host and port
+            parent_server = websockets.serve(
+                self.consumer_producer_handler,
+                self.config.get('PROFILER_HOST', self.DEFAULT_HOST),
+                self.config.get('PROFILER_PORT', self.DEFAULT_PORT)
+            ) 
+            asyncio.get_event_loop().\
+            run_until_complete(parent_server)
+            asyncio.get_event_loop().run_forever()
+
+
+  
+if __name__ == '__main__':
+    #create a isntance from the command line
+    profiler = ServerProfiler.create_from_cli_args()
+    profiler.run()
+
+    #print(profiler.__dict__)
+
